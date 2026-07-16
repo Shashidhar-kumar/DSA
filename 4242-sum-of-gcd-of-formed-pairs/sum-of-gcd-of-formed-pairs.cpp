fa5cfa7fe7e3c1c1a17ac7552fe43567778b2fd5
Shashidhar-kumar <billa.shashidhar7@gmail.com>
2026-07-16 13:12:58 +0530

    Time: 71 ms (73.12%) | Memory: 163 MB (13.36%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-int gcd(int a, int b) {
-    while (b != 0) {
-        int rem = a % b;
-        a = b;
-        b = rem;
-    }
-    return a;
-}
+// int gcd(int a, int b) {
+//     while (b != 0) {
+//         int rem = a % b;
+//         a = b;
+//         b = rem;
+//     }
+//     return a;
+// }
     long long gcdSum(vector<int>& nums) {
         vector<int>prefix;
         int i=0;
