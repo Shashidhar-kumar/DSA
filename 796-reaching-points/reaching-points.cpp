9f049970321b08a79bf62bd2df98d44ad7474054
Shashidhar-kumar <billa.shashidhar7@gmail.com>
2026-08-26 17:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (71.22%) - LeetSync

---
+++ 796-reaching-points/reaching-points.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx>sx && ty>sy){
+            if(tx>ty){
+                tx=tx%ty;
+            }
+            else{
+                ty=ty%tx;
+            }
+        }
+            if(tx==sx && ty>=sy){
+                return (ty-sy)%sx==0;
+            }
+            if(ty==sy && tx>=sx){
+                return (tx-sx)%sy==0;
+            }
+        return false;
+    }
+};
